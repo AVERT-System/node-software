7444be935635c160ddb195b972b3e9e1f1901497
Conor Bacon <conor.bacon@gmail.com>
2023-11-01 00:18:45 -0400

    :atom: Add Vaisala CO2 probe driver
    Add a driver for the Vaisala CO2 soil probe instrument in the collection
    of gas instruments drivers. The novac DOAS driver code was moved to
    a standalone module.

---
+++ data-acquisition/avert_system/drivers/gas/vaisala/__init__.py
@@ -0,0 +1,12 @@
+"""
+This module contains drivers for Vaisala gas instruments.
+
+:copyright:
+    2023, The AVERT System Team.
+:license:
+    GNU General Public License, Version 3
+    (https://www.gnu.org/licenses/gpl-3.0.html)
+
+"""
+
+from .gmp343 import query as query_gmp343  # NOQA
